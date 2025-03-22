51b88d5dc6604325f095e1a9f4d99c1d320fd9cd
yreyes3 <109697351+yreyes3@users.noreply.github.com>
2025-03-22 06:03:50 +0000

    Finalized HelloWorld Example

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,14 @@
+# make sure to install flask in terminal below with command: pip install Flask
+# command to run website: flask --app hello run
+
+# Import the Flask class
 from flask import Flask
 
+# Create a new Flask web app
+# __name__ is a special Python variable that tells Flask which file is being run
 app = Flask(__name__)
 
+# Tell Flask that when someone visits the homepage ("/"), run the following function
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-# run command: flask --app hello run
